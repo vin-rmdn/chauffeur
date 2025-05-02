12e5a8df5b784014e7c8aa487cfd472b43d1dfeb
Alvin Januar <aljan_r@outlook.co.id>
2025-05-03 02:50:55 +0700

    chore: initial commit

---
+++ app/src/main/java/chauffeur/Chauffeur.java
@@ -0,0 +1,14 @@
+package chauffeur;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableAutoConfiguration
+@RestController
+public class Chauffeur {
+    @GetMapping("/about")
+    public String about() {
+        return "Chauffeur is a Java application for setting up mock REST API with Spring, set up as a learning experience.";
+    }
+}
