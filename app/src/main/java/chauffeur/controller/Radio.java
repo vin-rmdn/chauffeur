95396054154bf691f5378131cc814fad334076be
Alvin Januar <aljan_r@outlook.co.id>
2026-01-16 15:30:00 +0700

    chore: change url + allow multiple day offsets

--- app/src/main/java/chauffeur/controller/Radio.java
+++ app/src/main/java/chauffeur/controller/Radio.java
@@ -1,10 +1,12 @@
 package chauffeur.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import chauffeur.radio.external.OnlineRadioBox;
@@ -21,13 +23,19 @@ public class Radio {
 
         public RadioPlaylistResponse(String id) {
             this.id = id;
+            this.playlist = new ArrayList<>();
         }
     }
 
-    @GetMapping("/radio/status/{id}")
-    public RadioPlaylistResponse GetRadioPlaylist(@PathVariable(name = "id") String id) throws Exception {
+    @GetMapping("/radio/playlists/{id}")
+    public RadioPlaylistResponse GetRadioPlaylist(
+        @PathVariable(name = "id") String id,
+        @RequestParam(value="day_offsets") List<Integer> dayOffsets
+    ) throws Exception {
         RadioPlaylistResponse response = new RadioPlaylistResponse(id);
-        response.playlist = this.onlineRadioBox.getPlaylist(id, 0);
+
+        for (Integer dayOffset : dayOffsets)
+            response.playlist.addAll(this.onlineRadioBox.getPlaylist(id, dayOffset));
 
         return response;
     }
