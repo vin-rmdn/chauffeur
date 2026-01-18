9b8d82f55e41c4ec5419d1743e1b7e2f6dc1e5fd
Alvin Januar <aljan_r@outlook.co.id>
2026-01-18 10:36:29 +0700

    feat(radio-service): title and artist filter

--- app/src/main/java/chauffeur/radio/RadioService.java
+++ app/src/main/java/chauffeur/radio/RadioService.java
@@ -2,6 +2,8 @@ package chauffeur.radio;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import chauffeur.radio.external.OnlineRadioBox;
@@ -10,13 +12,24 @@ import chauffeur.radio.external.OnlineRadioBox.SongRecord;
 @Service
 public class RadioService {
     OnlineRadioBox radioClient;
+    List<String> excludedArtists;
+    List<String> excludedTitles;
 
-    public RadioService(OnlineRadioBox radioClient) {
+    @Autowired
+    public RadioService(
+            OnlineRadioBox radioClient,
+            @Value("${radio.excluded_artists}") List<String> excludedArtists,
+            @Value("${radio.excluded_titles}") List<String> excludedTitles) {
         this.radioClient = radioClient;
+        this.excludedArtists = excludedArtists;
+        this.excludedTitles = excludedTitles;
     }
-    
+
     public List<SongRecord> GetPlaylists(String id, int dayOffset) throws Exception {
         List<SongRecord> songRecords = this.radioClient.getPlaylist(id, dayOffset);
+
+        songRecords.removeIf(rec -> this.excludedArtists.contains(rec.song.artist));
+        songRecords.removeIf(rec -> this.excludedTitles.contains(rec.song.title));
 
         return songRecords;
     }
--- app/src/test/java/chauffeur/radio/RadioServiceTest.java
+++ app/src/test/java/chauffeur/radio/RadioServiceTest.java
@@ -1,11 +1,15 @@
 package chauffeur.radio;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -22,16 +26,21 @@ public class RadioServiceTest {
     OnlineRadioBox mockRadioClient;
 
     @InjectMocks
-    RadioService classInTest = new RadioService(mockRadioClient);
+    RadioService classInTest = new RadioService(mockRadioClient, List.of("filtered-artist"), List.of("filtered-title"));
 
     @Test
     void TestRadioService_Successful() throws Exception {
-        List<SongRecord> mockResponse = Arrays.asList(
-                new SongRecord(
-                        new Song("artist", "title"), "20:26"));
+        SongRecord actualRecord = new SongRecord(new Song("artist", "title"), "20:26");
+
+        List<SongRecord> mockResponse = new ArrayList<SongRecord>();
+        mockResponse.add(actualRecord);
+        mockResponse.add(new SongRecord(new Song("filtered-artist", "title2"), "20:30"));
+        mockResponse.add(new SongRecord(new Song("artist2", "filtered-title"), "20:35"));
         when(mockRadioClient.getPlaylist("id", 1)).thenReturn(mockResponse);
 
         List<SongRecord> actual = classInTest.GetPlaylists("id", 1);
-        assertEquals(mockResponse, actual);
+
+        List<SongRecord> expected = Arrays.asList(actualRecord);
+        Assertions.assertArrayEquals(expected.toArray(), actual.toArray());
     }
 }
