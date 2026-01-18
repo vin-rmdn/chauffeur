520e1876773499ef72e2687930a74b26eea7a58c
Alvin Januar <aljan_r@outlook.co.id>
2026-01-18 08:48:46 +0700

    feat(radio): complete service

--- app/src/main/java/chauffeur/radio/RadioService.java
+++ app/src/main/java/chauffeur/radio/RadioService.java
@@ -1,7 +1,23 @@
 package chauffeur.radio;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import chauffeur.radio.external.OnlineRadioBox;
+import chauffeur.radio.external.OnlineRadioBox.SongRecord;
+
+@Service
 public class RadioService {
-    public void GetPlaylists() {
-        // TODO: implement
+    OnlineRadioBox radioClient;
+
+    public RadioService(OnlineRadioBox radioClient) {
+        this.radioClient = radioClient;
+    }
+    
+    public List<SongRecord> GetPlaylists(String id, int dayOffset) throws Exception {
+        List<SongRecord> songRecords = this.radioClient.getPlaylist(id, dayOffset);
+
+        return songRecords;
     }
 }
--- app/src/test/java/chauffeur/radio/RadioServiceTest.java
+++ app/src/test/java/chauffeur/radio/RadioServiceTest.java
@@ -1,12 +1,37 @@
 package chauffeur.radio;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import chauffeur.radio.external.OnlineRadioBox;
+import chauffeur.radio.external.OnlineRadioBox.Song;
+import chauffeur.radio.external.OnlineRadioBox.SongRecord;
+
+@ExtendWith(MockitoExtension.class)
 public class RadioServiceTest {
-    RadioService classInTest = new RadioService();
+    @Mock
+    OnlineRadioBox mockRadioClient;
+
+    @InjectMocks
+    RadioService classInTest = new RadioService(mockRadioClient);
 
     @Test
-    void TestRadioService_Successful() {
-        classInTest.GetPlaylists();
-    }   
+    void TestRadioService_Successful() throws Exception {
+        List<SongRecord> mockResponse = Arrays.asList(
+                new SongRecord(
+                        new Song("artist", "title"), "20:26"));
+        when(mockRadioClient.getPlaylist("id", 1)).thenReturn(mockResponse);
+
+        List<SongRecord> actual = classInTest.GetPlaylists("id", 1);
+        assertEquals(mockResponse, actual);
+    }
 }
