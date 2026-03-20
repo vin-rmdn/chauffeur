13be7d41fa23fc7dc8160bd20d59909dc383858e
Alvin Januar <aljan_r@outlook.co.id>
2026-03-20 22:48:01 +0700

    feat(discord): handle subscription

--- app/src/main/java/chauffeur/discord/Discord.java
+++ app/src/main/java/chauffeur/discord/Discord.java
@@ -1,11 +1,20 @@
 package chauffeur.discord;
 
+import java.sql.SQLException;
+import java.util.Optional;
+
+import chauffeur.discord.subscriber.Service;
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
+import discord4j.core.object.entity.User;
 import discord4j.core.object.entity.channel.MessageChannel;
 
 public class Discord {
-    public Discord() {}
+    Service service;
+
+    public Discord(Service service) {
+        this.service = service;
+    }
 
     public void handleMessageCreateEvent(MessageCreateEvent event) {
         Message message = event.getMessage();
@@ -17,7 +26,27 @@ public class Discord {
             case "hi":
                 channel.createMessage("https://nohello.net").block();
                 break;
-            // TODO: add more commands here
+            case "!subscribe":
+                Optional<User> user = message.getAuthor();
+                if (user.isEmpty()) {
+                    channel.createMessage("Error: no author is found").block();
+
+                    return;
+                }
+
+                try {
+                    service.subscribe(user.get().getId());
+                } catch (SQLException e) {
+                    channel.createMessage("Failed to subscribe: " + e.getMessage()).block();
+
+                    return;
+                }
+
+                channel.createMessage("Subscribed!").block();
+                break;
+
+            // Add more commands here
+
             default:
                 channel.createMessage("Lo siento, no entiendo ese comando.").block();
                 break;
