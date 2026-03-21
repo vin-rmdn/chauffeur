83a15ff5b225a637691560a27d0cc625dab5d6b3
Alvin Januar <aljan_r@outlook.co.id>
2026-03-21 22:09:00 +0700

    fix: remove unused Spring component

--- app/src/main/java/chauffeur/discord/Discord.java
+++ app/src/main/java/chauffeur/discord/Discord.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Component;
 
 import chauffeur.discord.subscriber.SubscribeService;
 import discord4j.core.DiscordClient;
