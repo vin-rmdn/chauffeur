35cd017539ebd3e613327b6c72fda418ce415bf5
Alvin Januar <aljan_r@outlook.co.id>
2026-03-23 00:31:18 +0700

    chore(radio): let DTO use records

--- app/src/main/java/chauffeur/controller/RadioController.java
+++ app/src/main/java/chauffeur/controller/RadioController.java
@@ -17,28 +17,23 @@ public class RadioController {
   @Autowired
   RadioService service;
 
-  public static class RadioPlaylistResponse {
-    public String id;
-    public Map<String, List<SongRecord>> playlists;
-
-    public RadioPlaylistResponse(String id) {
-      this.id = id;
-      this.playlists = new HashMap<>();
-    }
+  public record RadioPlaylistResponse(String id, Map<String, List<SongRecord>> playlists) {
   }
 
   @GetMapping("/radio/playlists/{id}")
   public RadioPlaylistResponse getRadioPlaylist(@PathVariable String id,
       @RequestParam(value = "day_offsets") List<Integer> dayOffsets) throws Exception {
-    RadioPlaylistResponse response = new RadioPlaylistResponse(id);
+
+    Map<String, List<SongRecord>> playlists = new HashMap<>();
 
     LocalDate today = LocalDate.now();
+
     for (Integer dayOffset : dayOffsets) {
       LocalDate playlistDate = today.minusDays((long) dayOffset);
 
-      response.playlists.put(playlistDate.toString(), this.service.getPlaylists(id, dayOffset));
+      playlists.put(playlistDate.toString(), this.service.getPlaylists(id, dayOffset));
     }
 
-    return response;
+    return new RadioPlaylistResponse(id, playlists);
   }
 }
